b32125f38bf4d6698c87759a2429f0903b64fad5
Thomas Kuschan <mail@thomaskuschan.de>
2023-10-30 12:35:21 +0100

    Implement magic register for de-/activation

--- src/Register.php
+++ src/Register.php
@@ -3,22 +3,41 @@
 namespace Devdot\Wordpress\Plugin\BasePlugin;
 
 abstract class Register {
-    private static string $plugin_name = ''; 
+    protected string $pluginName = '';
+    protected string $pluginFile = '';
 
     /**
      * @var array<string,callable>
      */
-    private array $hooks = [];
+    protected array $hooks = [];
+
+    public function __construct(string $pluginFile)
+    {
+        $this->pluginFile = $pluginFile;
+        $this->pluginName = basename($pluginFile, '.php');
+    }
 
     public function __invoke(): void
     {
         $this->registerHooks();
+        $this->registerMagicHooks();
     }
 
     protected function registerHooks(): void
     {
         foreach($this->hooks as $hook => $callable) {
-            add_action( $hook, $callable);
+            add_action($hook, $callable);
+        }
+    }
+
+    private function registerMagicHooks(): void
+    {
+        if(method_exists($this, 'activate')) {
+            register_activation_hook($this->pluginFile, [$this, 'activate']);
+        }
+
+        if(method_exists($this, 'deactivate')) {
+            register_deactivation_hook($this->pluginFile, [$this, 'deactivate']);
         }
     }
 }
